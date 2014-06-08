7fd691c944cc908a4270c89ca7b80f344916a4c2
Matt Dwen <matt@group6.co.nz>
2014-06-09 09:34:03 +1200

    GitHub Pages CNAME

--- Gruntfile.js
+++ Gruntfile.js
@@ -301,6 +301,7 @@ module.exports = function (grunt) {
             // Jekyll processes and moves HTML and text files.
             // Usemin moves CSS and javascript inside of Usemin blocks.
             // Copy moves asset files and directories.
+            'CNAME',
             'images/**/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
