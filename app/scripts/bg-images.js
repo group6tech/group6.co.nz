cfe8fb2b7a74c14fc92c545e3ded348400a85783
Matt Dwen <matt@group6.co.nz>
2015-09-18 08:28:44 +1200

    Global background image script

--- app/scripts/bg-images.js
+++ app/scripts/bg-images.js
@@ -1,6 +1,6 @@
 /*global $:true */
 $(function() {
-	$('.card-img').each(function() {
+	$('[data-src]').each(function() {
 		$(this).css('background-image', 'url(' + $(this).data('src') + ')');
 	});
 });
